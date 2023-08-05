62f790c3af02f3bcbf110083bd797932db563b84
mgracioli <michel-rg@hotmail.com>
2023-08-05 19:10:52 -0300

    Criação do método atualizar para a entidade Categoria

--- src/main/java/br/com/alura/loja/dao/CategoriaDao.java
+++ src/main/java/br/com/alura/loja/dao/CategoriaDao.java
@@ -14,4 +14,8 @@ public class CategoriaDao {
     public void cadastrar(Categoria categoria) {
         this.em.persist(categoria);
     }
+
+    public void atualizar(Categoria categoria) {
+        this.em.merge(categoria);   //o merge serve para colocar o objeto categoria no estado de "managed" pela JPA, caso ele não esteja nesse estado, o objeto não será atualizado no banco (a entidade sai da condição "managed" quando eu dou close() ou clear(0 na transação
+    }
 }

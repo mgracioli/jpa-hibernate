33b525919c6d6218d2adc9daf37651d78dbaddb2
mgracioli <michel-rg@hotmail.com>
2023-08-05 19:18:01 -0300

    Criação do método remover para a entidade Categoria

--- src/main/java/br/com/alura/loja/dao/CategoriaDao.java
+++ src/main/java/br/com/alura/loja/dao/CategoriaDao.java
@@ -16,6 +16,11 @@ public class CategoriaDao {
     }
 
     public void atualizar(Categoria categoria) {
-        this.em.merge(categoria);   //o merge serve para colocar o objeto categoria no estado de "managed" pela JPA, caso ele não esteja nesse estado, o objeto não será atualizado no banco (a entidade sai da condição "managed" quando eu dou close() ou clear(0 na transação
+        this.em.merge(categoria);   //o merge serve para colocar o objeto categoria no estado de "managed" pela JPA, caso ele não esteja nesse estado, o objeto não será atualizado no banco (a entidade sai da condição "managed" quando eu dou close() ou clear(0 na transação, não existe um método PUT na JPA pq, contanto que a entidade esteja no modo "managed", qualquer alteração nela dispara a atualização de forma automática
+    }
+
+    public void remover(Categoria categoria) {
+        categoria = this.em.merge(categoria);   //o merge coloca a entidade categoria no estado "managed". Só pode usar o método delete se a entidade estiver "managed"
+        this.em.remove(categoria);
     }
 }
